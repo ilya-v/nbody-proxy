72f8f985b1d598adf2d2742e1e23cc17f9fbd828
ilyav <ilya.volkovets@gmail.com>
2018-07-08 23:00:06 +0000

    cgi-bin scripts

--- install.sh
+++ install.sh
@@ -7,7 +7,13 @@ cd $SCRIPTPATH
 cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.$(date | tr ' ' -)
 ln -s -f $SCRIPTPATH/nginx.conf /etc/nginx/nginx.conf
 
-sed -i "s!set \$nbody_proxy_root.*!set \$nbody_proxy_root $SCRIPTPATH!g" nginx.conf
+mkdir -p /var/www/nbody-bin
+cp ./bin/* /var/www/nbody-bin
+chmod a+r-w /var/www/nbody-bin/*
+chmod a+x /var/www/nbody-bin/*
+chown -R www-data /var/www/nbody-bin
+chmod 750 /var/www/nbody-bin
+
 
 mkdir -p /var/www/nbody
 cp -R ./www/* /var/www/nbody
