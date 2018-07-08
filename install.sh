da597c493cc8fcf8a11d40a8910eb1a742289851
ilyav <ilya.volkovets@gmail.com>
2018-07-08 20:46:49 +0000

    Copy the static html into www dir

--- install.sh
+++ install.sh
@@ -5,7 +5,13 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $SCRIPTPATH
 
 cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.$(date | tr ' ' -)
-ln -s -f nginx.conf /etc/nginx/nginx.conf
+ln -s -f $SCRIPTPATH/nginx.conf /etc/nginx/nginx.conf
 
 sed -i "s!set \$nbody_proxy_root.*!set \$nbody_proxy_root $SCRIPTPATH!g" nginx.conf
 
+mkdir -p /var/www/nbody
+cp -R ./www/* /var/www/nbody
+chmod -R a+r-w /var/www/nbody
+chmod a+x /var/www/nbody
+
+chown -R www-data /var/www/nbody
